c543acb76c21914b11cdc47ba973ba944dce9ebb
flippinghell <39935719+flippinghell@users.noreply.github.com>
2019-10-17 21:15:11 +0800

    scripts

---
+++ project-zero-game-2/Assets/Scirpts/Enemy/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform target;
+    private Transform self;
+    public GameObject player;
+    private Rigidbody2D physics;
+    public float force = 10f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = player.GetComponent<Transform>();
+        self = GetComponent<Transform>();
+        physics = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = target.position.x - self.position.x;
+        float y = target.position.y - self.position.y;
+
+
+        physics.AddForce(new Vector2(x, y) / Mathf.Sqrt(x*x + y*y) * force);
+
+        print("my force vector to target is (" + x + ", " + y + ").");
+        
+    }
+}
---
+++ project-zero-game-2/Assets/Scirpts/Player/PlayerMovement2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+    private Rigidbody2D physics;
+    public float force = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        physics = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            physics.AddForce(Vector2.up * force);
+        if (Input.GetKey(KeyCode.S))
+            physics.AddForce(Vector2.down * force);
+        if (Input.GetKey(KeyCode.A))
+            physics.AddForce(Vector2.left * force);
+        if (Input.GetKey(KeyCode.D))
+            physics.AddForce(Vector2.right * force);
+    }
+}
