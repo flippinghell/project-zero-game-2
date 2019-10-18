95bdd5700090232db519faab2484010dc538cac9
Xreplayx <56397388+Xreplayx@users.noreply.github.com>
2019-10-18 21:49:08 +0800

    Punch

---
+++ project-zero-game-2/Assets/Scirpts/Player/Punching.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punching : MonoBehaviour
+{
+    public Transform PunchRPoint;
+    public GameObject FistRprefab;
+
+    public float FistRForce = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            PunchR();
+        }
+    }
+
+    void PunchR()
+    {
+        GameObject FistR = Instantiate(FistRprefab, PunchRPoint);
+        Rigidbody2D rb = FistR.GetComponent<Rigidbody2D>();
+        rb.AddForce(PunchRPoint.up * FistRForce, ForceMode2D.Impulse);
+    }
+}
