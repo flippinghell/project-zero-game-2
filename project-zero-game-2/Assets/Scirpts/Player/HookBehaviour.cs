bdf9921f01c0fc22ff69969abd44f43a54db1ed5
Xreplayx <56397388+Xreplayx@users.noreply.github.com>
2019-10-20 21:28:37 +0800

    Hook

---
+++ project-zero-game-2/Assets/Scirpts/Player/HookBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookBehaviour : MonoBehaviour
+{
+    public GameObject hiteffect;
+
+
+    void OnCollisionEnter2D(Collision2D Collision)
+    {
+        Destroy(gameObject);
+    }
+}
