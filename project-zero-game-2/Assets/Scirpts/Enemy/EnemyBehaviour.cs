899b49883e987b146b45dfc1d9ecfe1ad0039a6c
flippinghell <39935719+flippinghell@users.noreply.github.com>
2019-11-18 00:06:07 +0800

    enemy health and death

--- project-zero-game-2/Assets/Scirpts/Enemy/EnemyBehaviour.cs
+++ project-zero-game-2/Assets/Scirpts/Enemy/EnemyBehaviour.cs
@@ -7,6 +7,8 @@ public class EnemyBehaviour : MonoBehaviour
     public GameObject player;
     private EnemyMovement movement;
     private PlayerBehaviour PlayerBehaviour;
+    public int EnemyHealth = 2;
+    public GameObject fist;
     
 
     // Start is called before the first frame update
@@ -16,18 +18,41 @@ public class EnemyBehaviour : MonoBehaviour
         movement = GetComponent<EnemyMovement>();
         PlayerBehaviour = player.GetComponent<PlayerBehaviour>();
         
+        
     }
 
     void OnTriggerEnter2D (Collider2D other)
     {
         if (other.gameObject == player)
-        {
+        {   
             
             PlayerBehaviour.TakeDamage();
         }
 
+
         
         
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("fist"))
+        {
+            TakeDamage();
+            print("damaged");
+        }
+    }
+    void TakeDamage()
+    {
+        EnemyHealth--;
+
+        if (EnemyHealth <= 0)
+            Death();
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
